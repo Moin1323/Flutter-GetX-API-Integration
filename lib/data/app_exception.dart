9d404137a02162415b137ee946d733310cdc680f
Moin1323 <mueenaly.official@gmail.com>
2024-10-04 18:05:07 +0500

    EXCEPTIONS | exceptions handled successfully

---
+++ lib/data/app_exception.dart
@@ -0,0 +1,24 @@
+class AppException implements Exception {
+  final _message;
+  final _prefix;
+
+  AppException([this._message, this._prefix]);
+
+  @override
+  String toString() {
+    return '$_prefix$_message';
+  }
+}
+
+class InternetException extends AppException {
+  InternetException([String? message]) : super(message, 'No Internet');
+}
+
+class RequestTimOut extends AppException {
+  RequestTimOut([String? message]) : super(message, 'Request Timeout');
+}
+
+class ServerException extends AppException {
+  ServerException([String? message])
+      : super(message, 'Response from server failed');
+}
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:getx_api/data/app_exception.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          InternetException();
+        },
+      ),
+    );
+  }
+}
